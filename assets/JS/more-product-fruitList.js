9563158fb39d8bb2adb04a7a7c38c08bcdb3f774
levatrung14032004 <levantrung14032004@gmailcom>
2023-12-07 17:28:15 +0700

    xong cart content khi hover

--- assets/JS/more-product-fruitList.js
+++ assets/JS/more-product-fruitList.js
@@ -251,6 +251,77 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// Detele item
+function btnCancelCardContent(event) {
+  const index = event.currentTarget.getAttribute("data-index");
+  productsInCart.splice(index, 1);
+  localStorage.setItem("products", JSON.stringify(productsInCart));
+  var cartQuantity = document.querySelector(".cart__quantity");
+  var cartQuantityValue = 0;
+  productsInCart.forEach((item) => {
+    cartQuantityValue += item.soluong;
+  });
+  cartQuantity.innerHTML = cartQuantityValue;
+  rendercartContent();
+}
+
+// Cart content
+function rendercartContent() {
+  let cartContent = document.querySelectorAll(".cart__content-list.p-0");
+  cartContent[0].innerHTML = "";
+  let htmls = productsInCart.map((item, index) => {
+    return `
+                  <li class="cart__content-item d-flex px-3">
+                    <div class="content__item-img me-3">
+                      <img
+                        src="${item.imgSrc}"
+                        alt="${item.name}"
+                      />
+                    </div>
+
+                    <div class="content__item-details">
+                      <div class="content__item-name">
+                        <a href=""> ${item.name}</a>
+                      </div>
+
+                      <div class="">
+                        <span class="content__item-price">${item.price}</span>
+                        <span class="content__item-quantity">x${item.soluong}</span>
+                      </div>
+                    </div>
+
+                    <div  class="content__item-delete">
+                      <div  data-index="${index}" class="content__item-delete--btn text-end">
+                        <i class="fa-solid fa-xmark"></i>
+                      </div>
+                    </div>
+                  </li>
+      `;
+  });
+  cartContent[0].innerHTML = htmls.join("");
+
+  let deleteButtons = document.querySelectorAll(".content__item-delete--btn");
+  deleteButtons.forEach((button, index) => {
+    button.addEventListener("click", btnCancelCardContent);
+  });
+
+  // Render Total
+  function renderTotalPrice() {
+    var total = 0;
+    productsInCart.forEach((item) => {
+      total += item.soluong * item.price;
+    });
+    var resultFomatted = new Intl.NumberFormat().format(total);
+    document.querySelectorAll(
+      "div.cart__content-total.py-2.d-flex.align-items-center b.ms-2"
+    )[0].innerHTML = `${resultFomatted}₫`;
+  }
+
+  renderTotalPrice();
+}
+
+rendercartContent();
+
 function logOut() {
   let btnLogOut = document.querySelector("a.btn-logOut");
   btnLogOut.addEventListener("click", () => {
